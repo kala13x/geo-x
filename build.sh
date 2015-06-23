3472e6ab6e93c519370e1291e01b231975384451
Sun Dro <kala0x13@gmail.com>
2015-06-23 13:33:40 +0400

    Build script checks we can install or not

--- build.sh
+++ build.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 # Simple build script to compile project
 
+# Build Utils
 cd ${PWD##}/utils
 make clean
 make
 
+# Build GXX
 cd ../gxx
 make clean
 make
 
-echo "საჭიროა სუპერ მომხმარებლის პრივილეგიები"
+# Check we can run sudo command or not
+CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1 | grep "load" | wc -l)
+if [ ${CAN_I_RUN_SUDO} -gt 0 ]
+then
+    echo "Installing..."
+else
+    echo "You must have a root access"
+fi
+
+# Install GXX
 sudo make install
 
+# Install Libx
 cd ../libx
 sudo make install
 
 cd ..
+echo "Done"
